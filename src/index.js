66556a024f28dff282d844291123846283f05afb
aarya <aarya@switchfly.com>
2020-04-29 19:30:35 +0530

    wip: publishing post app

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+
+import './index.scss';
+import store from './store';
+import App from './containers/App/App';
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'),
+);
